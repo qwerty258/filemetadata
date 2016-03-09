4f1b05bc99b3fdbe4747c96c11b659700c507ea7
qwerty258 <zyf26256@gmail.com>
2016-03-09 11:03:23 +0800

    add batch test code

--- IDNumberProcess/IDNumberProcess.c
+++ IDNumberProcess/IDNumberProcess.c
@@ -1,30 +1,59 @@
 #include <stdio.h>
 #include "IDNumber.h"
 
-#define test ID2
-
 int main(int argc, char* argv[])
 {
-    char* ID1 = "330183199008250017";
-    char* ID2 = "daiiioda";
+    char* IDs[] = {
+        "330183199008250017",
+        "daiiiodaghjkghhhhh",
+        "33018319900825001X",
+        "dsafdak",
+        "51140019861016781x",
+        "5114dun9861016781x"
+    };
+
     char* temp = NULL;
-    int reuslt = IDN_RESULT_OK;
+    int result = IDN_RESULT_OK;
+    size_t total_tests = sizeof(IDs) / sizeof(char*);
+    ID_number_handle handle = IDN_get_handle("");
 
-    ID_number_handle handle = IDN_get_handle(test);
+    for(size_t i = 0; i < total_tests; i++)
+    {
+        printf("Test: %s\n", IDs[i]);
 
-    IDN_process_ID(handle);
+        IDN_reset_handle(handle, IDs[i]);
 
-    IDN_get_area_code(handle, &temp);
-    printf("area code: %s\n", temp);
+        result = IDN_process_ID(handle);
+        switch(result)
+        {
+            case IDN_RESULT_OK:
+                IDN_get_area_code(handle, &temp);
+                printf("area code: %s\n", temp);
 
-    IDN_get_birthdate(handle, &temp);
-    printf("birth date: %s\n", temp);
+                IDN_get_birthdate(handle, &temp);
+                printf("birth date: %s\n", temp);
 
-    IDN_get_three_NUM(handle, &temp);
-    printf("three num: %s\n", temp);
+                IDN_get_order_number(handle, &temp);
+                printf("order num: %s\n", temp);
 
-    IDN_get_check(handle, &temp);
-    printf("check: %s\n", temp);
+                IDN_get_check(handle, &temp);
+                printf("check: %s\n", temp);
+                break;
+            case IDN_WRONG_LEN:
+                printf("IDN_WRONG_LEN\n");
+                break;
+            case IDN_NOT_DIGIT:
+                printf("IDN_NOT_DIGIT\n");
+                break;
+            case IDN_INVALID_ID:
+                printf("IDN_INVALID_ID\n");
+                break;
+            default:
+                break;
+        }
+
+        printf("\n\n");
+    }
 
     IDN_free_handle(handle);
 
