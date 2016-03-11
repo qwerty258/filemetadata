1dbd0dbca105671f0c25edb3da2f5440ce6c8282
qwerty258 <zyf26256@gmail.com>
2016-03-11 17:34:25 +0800

    add code to use mysql connector

--- ID2MySQL/ID2MySQL.h
+++ ID2MySQL/ID2MySQL.h
@@ -5,9 +5,15 @@
 extern "C" {
 #endif // !__cplusplus
 
+    int initial_mysql_connector(void);
 
+    int connect_mysql_connector(char* host, char* username, char* password, char* db, unsigned int port);
 
+    int find_an_ID_mysql_connector(char* area_code, char* birth_date, char* order_number, char* check_number);
 
+    int insert_mysql_connector(char* area_code, char* birth_date, char* order_number, char* check_number);
+
+    int uninitial_mysql_connector(void);
 
 #ifdef __cplusplus
 }
