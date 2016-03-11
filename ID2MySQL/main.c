15b2afa5b7991e560f65681a98d40b31ce533de9
qwerty258 <zyf26256@gmail.com>
2016-03-11 09:25:41 +0800

    break down code to functions

--- ID2MySQL/main.c
+++ ID2MySQL/main.c
@@ -14,14 +14,44 @@ void get_rid_of(char* string)
     }
 }
 
+void spilt_buffer(char* buffer, char* name, char* ID)
+{
+    size_t i;
+    size_t j;
+    for(i = 0, j = 0; i < 1024; i++, j++)
+    {
+        if(',' != buffer[i])
+        {
+            name[j] = buffer[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+    i++;
+    for(j = 0; i < 1024; i++, j++)
+    {
+        if('\0' != buffer[i])
+        {
+            ID[j] = buffer[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    get_rid_of(name);
+    get_rid_of(ID);
+}
+
 int main(int argc, char* argv[])
 {
     FILE* p_file = fopen("D:\\Names and ID.csv", "r");
     char* buffer = calloc(1, 1024);
     char* name = calloc(1, 1024);
     char* ID = calloc(1, 1024);
-    size_t i;
-    size_t j;
     ID_number_handle handle;
     int result;
     char* temp;
@@ -30,38 +60,12 @@ int main(int argc, char* argv[])
     {
         while(NULL != fgets(buffer, 1024, p_file))
         {
-            for(i = 0, j = 0; i < 1024; i++, j++)
-            {
-                if(',' != buffer[i])
-                {
-                    name[j] = buffer[i];
-                }
-                else
-                {
-                    break;
-                }
-            }
-            i++;
-            for(j = 0; i < 1024; i++, j++)
-            {
-                if('\0' != buffer[i])
-                {
-                    ID[j] = buffer[i];
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            get_rid_of(name);
-            get_rid_of(ID);
+            spilt_buffer(buffer, name, ID);
             IDN_reset_handle(handle, ID);
             result = IDN_process_ID(handle);
             if(0 != result)
             {
                 printf("ID INVALID\n");
-                getchar();
             }
             else
             {
