2ae4d5206c8f71e1201dc9bc73383dc92af506ff
yaofei zheng <zyf26256@gmail.com>
2023-07-09 21:54:55 +0800

    add structs of torrent, serial and book metadata define

--- datastructdefines.h
+++ datastructdefines.h
@@ -19,4 +19,78 @@ typedef struct
     QString sha1;
 } corrupted_file_t;
 
+typedef struct
+{
+    QString path;
+    quint64 length;
+} file_in_torrent;
+
+typedef struct
+{
+    QString name;
+    QString url;
+    QString publisher;
+    QString created_by;
+    QString creation_date;
+    quint32 piece_length;
+    QString info_hash_v1;
+    QString info_hash_v2;
+    QVector<file_in_torrent>;
+} torrent_metadata_t;
+
+typedef struct
+{
+    QString author;
+    QString year;
+    QString title;
+    QString journal;
+    QString volume;
+    QString issue;
+    QString pages;
+    QString start_page;
+    QString issn;
+    QString doi;
+    QString keywords;
+    QString abstract;
+    QString author_address;
+    QString language;
+} serial_metadata_t;
+
+typedef struct
+{
+    QString author;
+    QString secondary_author;
+    QString subsidiary_author;
+    QString year;
+    QString title;
+    QString secondary_title;
+    QString pages;
+    QString volume;
+    QString number;
+    QString number_of_volumes;
+    QString place_published;
+    QString publisher;
+    QString edition;
+    QString keywords;
+    QString abstract;
+    QString url;
+    QString tertiary_title;
+    QString tertiary_author;
+    QString isbn;
+    QString chinese_unified_book_number;
+    QString alternate_title;
+    QString accession_number;
+    QString call_number;
+    QString short_title;
+    QString section;
+    QString original_publication;
+    QString reprint_edition;
+    QString electronic_resource_number;
+    QString translated_author;
+    QString translated_title;
+    QString language;
+    QString record_properties;
+    QString record_last_updated;
+} book_metadata_t;
+
 #endif // DATASTRUCTDEFINES_H
